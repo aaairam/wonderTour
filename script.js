d1dab77ddee9fe319720b14f988ef1cef137d38a
aaairam <magildaceria@gmail.com>
2023-03-22 15:50:07 +0800

    mariaaaa

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__mobile__nav");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
+});
